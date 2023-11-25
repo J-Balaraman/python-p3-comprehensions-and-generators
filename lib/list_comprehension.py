aa5439c351da65af99ed728d5b75bd886a862f49
jbalaraman <james.balaraman@gmail.com>
2023-11-24 17:11:09 -0700

    Implemented basic list compression in Python

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if (n % 2) == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamtion_list = [f"{sentence_list[n]}!" for n in range(len(sentence_list)) if n != ""]
+    return exclamtion_list
